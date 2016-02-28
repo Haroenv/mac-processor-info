f6089cfe7f15e83a5a44824538dce007fa157b89
Haroen Viaene <hello@haroen.me>
2016-02-28 12:18:07 +0100

    use simplified version

--- processor-info.sh
+++ processor-info.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-open http://ark.intel.com/search?q=$(sysctl -n machdep.cpu.brand_string | awk '{FS=" " ; print $3}')
+open http://ark.intel.com/search?q=$(sysctl -n machdep.cpu.brand_string | awk '{print $3}')
